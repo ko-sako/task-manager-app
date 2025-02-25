513478126bcd7743d65cb77bfdbbd345c41207db
Kayo Osako <osakok@cardiff.ac.uk>
2025-02-25 02:34:51 +0000

    Implement endpoint for get tasks by id

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from database import tasks_collection
 
 app = Flask(__name__)
 
@@ -6,5 +7,13 @@ app = Flask(__name__)
 def home():
     return {"message": "Task Manager API"}, 200
 
+@app.route("/tasks", methods=["GET"])
+def get_tasks():
+    tasks = list(tasks_collection.find())
+    for task in tasks:
+        task["_id"] = str(task["_id"])
+    return jsonify(tasks)
+
 if __name__ == "__main__":
     app.run(debug=True)
+
