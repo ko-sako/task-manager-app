87b43d81ddd105a49d0208cfd6f1f3f6be7e6389
Kayo Osako <osakok@cardiff.ac.uk>
2025-02-25 01:12:43 +0000

    add test for get task by id

--- backend/tests/test_database.py
+++ backend/tests/test_database.py
@@ -1,6 +1,15 @@
 from database import tasks_collection
 
 def test_insert_task():
-    task = {"title": "First tests", "done": False}
+    task = {"title": "Insert tests", "done": False}
     result = tasks_collection.insert_one(task)
     assert result.acknowledged == True
+
+def test_get_task_by_id():
+    task = {"title": "Get Task Test", "done": False}
+    result = tasks_collection.insert_one(task)
+    task_id = result.inserted_id
+
+    retrived_task = tasks_collection.find_one({"_id": task_id})
+    assert retrived_task is not None
+    assert retrived_task["title"] == "Get Task Test"
